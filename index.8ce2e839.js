b7c48c785fe0a74bdb37fac691848ca9c5fd32dd
YanaKarpliuk <91073484+YanaKarpliuk@users.noreply.github.com>
2022-05-28 22:27:18 +0000

    Deploying to gh-pages from @ YanaKarpliuk/icecream-team-11@fd9e9957c96cd944e0767d24198037c071e804d1 🚀

--- index.8ce2e839.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-active"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))})();
-//# sourceMappingURL=index.8ce2e839.js.map
--- index.c3ea18be.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-active"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.c3ea18be.js.map
