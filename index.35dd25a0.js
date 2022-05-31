c23b02a20d0a3deb1a49d7f8bea1f69a3ba11dca
YanaKarpliuk <91073484+YanaKarpliuk@users.noreply.github.com>
2022-05-31 19:22:28 +0000

    Deploying to gh-pages from @ YanaKarpliuk/icecream-team-11@60dde8beae43e817bca31eb3cb10b07de8be56c2 🚀

--- index.35dd25a0.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.35dd25a0.js.map
--- index.e401e5ca.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){document.body.classList.toggle("modal-open"),e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.e401e5ca.js.map
